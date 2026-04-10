b84c996c774a9cb17278b177cdacb90b1187622b
anuroop0003 <anuroop0003@gmail.com>
2026-04-10 15:26:57 +0530

    feat: initial commit

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -5,23 +5,95 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
+import { BellIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function DashboardLayout() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+        <header className="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 bg-primary px-4">
+          <div className="ml-auto flex items-center gap-4">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="relative hover:bg-transparent cursor-pointer"
+            >
+              <BellIcon className="size-5 text-background" />
+              <span className="absolute animate-pulse right-1 top-1 flex size-2 rounded-full bg-red-600"></span>
+            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger
+                render={
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 rounded-full"
+                  >
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage src="/avatars/shadcn.jpg" alt="@shadcn" />
+                      <AvatarFallback>SC</AvatarFallback>
+                    </Avatar>
+                  </Button>
+                }
+              />
+              <DropdownMenuContent className="w-56" align="end">
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">shadcn</p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        m@example.com
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem className="cursor-pointer rounded-sm">
+                    Profile
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer rounded-sm">
+                    Billing
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer rounded-sm">
+                    Settings
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer rounded-sm">
+                    New Team
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup className="flex items-center justify-between px-3 py-1.5">
+                  <DropdownMenuItem className="size-6 cursor-pointer rounded-xs bg-slate-950 hover:bg-slate-950! hover:scale-105" />
+                  <DropdownMenuItem className="size-6 cursor-pointer rounded-xs bg-blue-600 hover:bg-blue-600! hover:scale-105" />
+                  <DropdownMenuItem className="size-6 cursor-pointer rounded-xs bg-emerald-600 hover:bg-emerald-600! hover:scale-105" />
+                  <DropdownMenuItem className="size-6 cursor-pointer rounded-xs bg-orange-500 hover:bg-orange-500! hover:scale-105" />
+                  <DropdownMenuItem className="size-6 cursor-pointer rounded-xs bg-violet-600 hover:bg-violet-600! hover:scale-105" />
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="cursor-pointer rounded-sm hover:bg-red-300! hover:text-red-700!">
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </header>
-        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <Outlet />
-        </div> */}
-        <div className="relative h-full w-full bg-white">
-          <Outlet />
-          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <div className="relative flex-1 bg-white">
+          <div className="relative z-0">
+            <Outlet />
+          </div>
+          <div className="pointer-events-none absolute inset-0 z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
         </div>
       </SidebarInset>
     </SidebarProvider>
