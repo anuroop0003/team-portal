4681d44f00dfb541485792698b14d47e53c296d5
anuroop0003 <anuroop0003@gmail.com>
2026-06-04 18:48:38 +0530

    fix: revert authentication bypass in private and public route guards to use local storage token

--- src/routes/guards/private-route.tsx
+++ src/routes/guards/private-route.tsx
@@ -2,8 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { PATHS } from "../constants/paths";
 
 export const PrivateRoute = () => {
-  // const isAuthenticated = localStorage.getItem("access-token");
-  const isAuthenticated = true;
+  const isAuthenticated = localStorage.getItem("access-token");
 
   if (!isAuthenticated) {
     return <Navigate to={PATHS.AUTH.SIGN_IN} replace />;
